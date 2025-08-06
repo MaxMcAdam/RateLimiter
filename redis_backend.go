4079ca3667fee156209b447a69de1348e4627180
MaxMcAdam <mmax@fredcom.com>
2025-08-06 17:25:05 -0400

    Wrote redis backend scaffolding

--- redis_backend.go
+++ redis_backend.go
@@ -1,13 +1,41 @@
 package ratelimiter
 
 import (
-	"github.com/go-redis/redis/v8"
+	"time"
+
+	redis "github.com/redis/go-redis/v9"
 )
 
-type RedisBackend struct {
+type RedisBackend interface {
+	CheckAndIncrementBucket(key string) (bool, error)
+}
+
+type SingleRedisBackend struct {
 	client *redis.Client
 }
 
-func NewRedisBackend(config *RLConfig) RedisBackend {
-	return RedisBackend{}
+func NewSingleRedisBackend(config *RLConfig) SingleRedisBackend {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password
+		DB:       0,  // default DB
+
+		// Connection pool settings
+		PoolSize:        10,               // Max connections in pool
+		MinIdleConns:    2,                // Min idle connections to maintain
+		MaxIdleConns:    5,                // Max idle connections
+		ConnMaxIdleTime: 30 * time.Minute, // Close idle connections after 30min
+		ConnMaxLifetime: time.Hour,        // Close connections after 1 hour
+
+		// Timeouts
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+
+		// Retries
+		MaxRetries:      3,
+		MinRetryBackoff: 8 * time.Millisecond,
+		MaxRetryBackoff: 512 * time.Millisecond,
+	})
+	return SingleRedisBackend{client}
 }
